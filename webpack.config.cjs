d4298d4e822630328957730b75d3729ac0706d5e
Kerry Shetline <kerry@shetline.com>
2021-05-11 19:36:20 -0400

    Update es5/6 packaging.

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,21 +1,44 @@
+const TerserPlugin = require('terser-webpack-plugin');
 const { resolve } = require('path');
 
 module.exports = env => {
+  const esVersion = env?.esver === '5' ? 'es5' : 'es6';
+  const dir = env?.esver === '5' ? 'web5' : 'web';
+  const chromeVersion = env?.esver === '5' ? '23' : '51';
+
   return {
     mode: env?.dev ? 'development' : 'production',
-    target: ['web', 'es5'],
+    target: [esVersion, 'web'],
     entry: './dist/index.js',
     output: {
-      path: resolve(__dirname, 'dist/web'),
+      path: resolve(__dirname, 'dist/' + dir),
       filename: 'index.js',
       libraryTarget: 'umd',
       library: 'tbABR'
     },
     module: {
       rules: [
-        { test: /\.js$/, use: 'babel-loader', resolve: { fullySpecified: false } }
+        {
+          test: /\.js$/,
+          use: {
+            loader: 'babel-loader',
+            options: { presets: [['@babel/preset-env', { targets: { chrome: chromeVersion } }]] }
+          },
+          resolve: { fullySpecified: false }
+        }
       ]
     },
-    devtool: 'source-map'
+    optimization: {
+      minimize: !env?.dev,
+      minimizer: [new TerserPlugin({
+        terserOptions: {
+          output: { max_line_len: 511 }
+        }
+      })],
+    },
+    devtool: 'source-map',
+    resolve: {
+      mainFields: ['es2015', 'browser', 'module', 'main', 'main-es5']
+    }
   };
 };
