2a61512852b16fc23700a7183c1885d041432434
Kerry Shetline <kerry@shetline.com>
2021-06-21 18:38:04 -0400

    Update to new hybrid ES6/ES2018 packaging.

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,27 +2,45 @@ const TerserPlugin = require('terser-webpack-plugin');
 const { resolve } = require('path');
 
 module.exports = env => {
-  const esVersion = env?.esver === '5' ? 'es5' : 'es6';
-  const dir = env?.esver === '5' ? 'web5' : 'web';
-  const chromeVersion = env?.esver === '5' ? '23' : '51';
+  const umd = !!env?.umd && (/^[ty]/i.test(env?.umd) || Number(env?.umd) !== 0);
+  const cjs = !umd && !!env?.cjs && (/^[ty]/i.test(env?.cjs) || Number(env?.cjs) !== 0);
+  const esVersion = umd ? 'es6' : 'es2018';
+  const dir = umd ? 'web' : (cjs ? 'cjs' : 'fesm2015');
+  const libraryTarget = umd ? 'umd' : (cjs ? 'commonjs' : 'module');
+  const asModule = !umd && !cjs;
+  const outFile = `index.${asModule ? 'm' : ''}js`;
 
   return {
     mode: env?.dev ? 'development' : 'production',
     target: [esVersion, 'web'],
     entry: './dist/index.js',
+    experiments: {
+      outputModule: asModule
+    },
     output: {
-      path: resolve(__dirname, 'dist/' + dir),
-      filename: 'index.js',
-      libraryTarget: 'umd',
-      library: 'tbABR'
+      path: resolve(__dirname, 'dist', dir),
+      filename: outFile,
+      libraryTarget,
+      library: umd ? 'tbABR' : undefined
     },
     module: {
       rules: [
         {
           test: /\.js$/,
+          exclude: /\.spec\.js$/,
           use: {
             loader: 'babel-loader',
-            options: { presets: [['@babel/preset-env', { targets: { chrome: chromeVersion } }]] }
+            options: {
+              presets: [['@babel/preset-env', {
+                targets: { // min ES6 : min ES2018
+                  chrome:  umd ? '58' : '64',
+                  edge:    umd ? '14' : '79',
+                  firefox: umd ? '54' : '78',
+                  opera:   umd ? '55' : '51',
+                  safari:  umd ? '10' : '12',
+                }
+              }]]
+            }
           },
           resolve: { fullySpecified: false }
         }
@@ -36,9 +54,6 @@ module.exports = env => {
         }
       })],
     },
-    devtool: 'source-map',
-    resolve: {
-      mainFields: ['es2015', 'browser', 'module', 'main', 'main-es5']
-    }
+    devtool: 'source-map'
   };
 };
